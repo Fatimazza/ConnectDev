f18632080bae00be4a2ef35bb76df2406d9893b1
Rio Swarawan Putra <swarawan.rio@gmail.com>
2017-11-08 05:34:36 +0700

    [rioswarawan] add login and assets

---
+++ app/src/main/java/id/co/binar/connectdev/App.java
@@ -0,0 +1,15 @@
+package id.co.binar.connectdev;
+
+import android.app.Application;
+
+/**
+ * Created by rioswarawan on 11/7/17.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
