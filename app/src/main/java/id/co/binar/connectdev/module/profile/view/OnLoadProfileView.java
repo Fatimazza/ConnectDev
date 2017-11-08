74783eda4df021769d37e468330f34814816381d
Fatima Azzahro <fatima@paypro.id>
2017-11-08 12:27:03 +0700

    [fatimazza] Profile - Add and call Interface for Profile View

---
+++ app/src/main/java/id/co/binar/connectdev/module/profile/view/OnLoadProfileView.java
@@ -0,0 +1,13 @@
+package id.co.binar.connectdev.module.profile.view;
+
+/**
+ * Created by fatimazza on 11/8/17.
+ */
+
+public interface OnLoadProfileView {
+
+    void onLoadProfileSucces();
+
+    void onLoadProfileError(String message);
+
+}
--- app/src/main/java/id/co/binar/connectdev/module/profile/view/ProfileActivity.java
+++ app/src/main/java/id/co/binar/connectdev/module/profile/view/ProfileActivity.java
@@ -2,6 +2,7 @@ package id.co.binar.connectdev.module.profile.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import id.co.binar.connectdev.R;
 
@@ -13,4 +14,16 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
     }
 
+    private OnLoadProfileView loadProfileView = new OnLoadProfileView() {
+        @Override
+        public void onLoadProfileSucces() {
+            // TODO: 11/8/17 Show Profile
+            Toast.makeText(ProfileActivity.this, "Success", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onLoadProfileError(String message) {
+
+        }
+    };
 }
