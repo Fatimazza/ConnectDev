fd6b3f8d5dac097bf085e643c1a4fa04e8a05f84
Rio Swarawan Putra <swarawan.rio@gmail.com>
2017-11-08 13:05:25 +0700

    [rioswarawan] fix login facebook

--- app/src/main/java/id/co/binar/connectdev/module/login/view/LoginActivity.java
+++ app/src/main/java/id/co/binar/connectdev/module/login/view/LoginActivity.java
@@ -1,5 +1,6 @@
 package id.co.binar.connectdev.module.login.view;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,55 +9,79 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.util.Arrays;
+
 import id.co.binar.connectdev.R;
-import id.co.binar.connectdev.module.login.presenter.LoginPresenter;
 import id.co.binar.connectdev.module.main.view.MainActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ImageView facebookButton;
 
-    private LoginPresenter presenter;
+    private String[] permissions = {"public_profile"};
+    private CallbackManager facebookCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        presenter = new LoginPresenter(this, loginView);
-
         facebookButton = (ImageView) findViewById(R.id.facebook_login_button);
         facebookButton.setOnClickListener(onFacebookClicked);
+
+        facebookCallbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(facebookCallbackManager, facebookCallback);
     }
 
     @Override
-    protected void onDestroy() {
-        presenter.onDestroy();
-        super.onDestroy();
+    protected void onStart() {
+        super.onStart();
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        if (accessToken != null) {
+            routeToMain();
+        }
     }
 
     private View.OnClickListener onFacebookClicked = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            presenter.facebookSignIn();
+            LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList(permissions));
         }
     };
 
-    private OnLoginView loginView = new OnLoginView() {
+    private FacebookCallback<LoginResult> facebookCallback = new FacebookCallback<LoginResult>() {
         @Override
-        public void onLoginSuccess() {
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
+        public void onSuccess(LoginResult loginResult) {
+            routeToMain();
         }
 
         @Override
-        public void onError(String message) {
-            Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+        public void onCancel() {
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+            Toast.makeText(LoginActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
         }
     };
+
+    private void routeToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            facebookCallbackManager.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
